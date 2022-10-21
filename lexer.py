9fd9e3f1e94d6d4492b81884b86fc2886ac558f7
martysh12 <martyshkon@gmail.com>
2022-10-22 01:15:03 +0300

    Fix token order

--- lexer.py
+++ lexer.py
@@ -1,17 +1,32 @@
 from sly import Lexer
 
 class LimScriptLexer(Lexer):
-    tokens = { TOKEN_ID, TOKEN_SEPARATOR, TOKEN_NUMBER, TOKEN_ASCII, TOKEN_VAR, TOKEN_EQUALS }
+    # The order is important!
+    tokens = {
+        TOKEN_PRINT,
+        TOKEN_VAR,
+        TOKEN_ID, 
+        TOKEN_ASCII,
+        TOKEN_NUMBER,
+        TOKEN_EQUALS,
+        TOKEN_SEPARATOR
+    }
 
     ignore = " \t\n"
 
 
+    TOKEN_PRINT = r'print'
     TOKEN_VAR = r'var'
+    
     TOKEN_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    TOKEN_SEPARATOR = r';'
-    TOKEN_NUMBER = r'\d+'
+
+
     TOKEN_ASCII = r'#[\x21-\x7E]'
+    TOKEN_NUMBER = r'\d+'
+
     TOKEN_EQUALS = r'='
+    
+    TOKEN_SEPARATOR = r';'
 
 if __name__ == '__main__':
     data = """
